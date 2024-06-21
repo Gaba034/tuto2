613dfd024f21f4a1b2379c5f5677ba95c6c2339b
G7K9 <gabiiinski@hotmail.com>
2024-06-21 19:02:31 -0300

    public

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Boots',
+            description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel urna pellentesque, varius erat eu, ullamcorper quam. Vivamus tincidunt facilisis tellus, eu rhoncus felis scelerisque sed. Fusce aliquet, ex vel viverra ullamcorper, enim tortor hendrerit massa, sed ornare ligula lectus mollis est. Ut non ante est. Sed vestibulum quis purus viverra faucibus. Pellentesque ut turpis ultricies leo suscipit pellentesque quis in enim. Duis vulputate sed neque vel tristique. Suspendisse fringilla risus nunc, at finibus lacus finibus in. Duis mauris mauris, varius et purus vitae, venenatis tincidunt elit. Donec eleifend auctor tellus dignissim convallis. Nam ex odio, sodales sit amet mattis eu, tincidunt vel nibh. Pellentesque rhoncus enim ac luctus tristique. Ut volutpat hendrerit neque at vehicula. Donec varius orci a neque semper, vel egestas tortor vestibulum. Proin eu felis dolor.'
+        }
+    }
+})
